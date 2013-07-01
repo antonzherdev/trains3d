e1e09a3d77267075da4be6cb4d264ebd79391a7e
antonzherdev <anton.zherdev@gmail.com>
2013-07-01 18:15:53 +0400

    city draw

--- Trains3D/Trains3D/View/TRCityView.m
+++ Trains3D/Trains3D/View/TRCityView.m
@@ -15,9 +15,29 @@
 - (void)drawCity:(TRCity*)city {
     glPushMatrix();
     glTranslatef(city.tile.x, city.tile.y, 0);
+    glTranslatef(0, 0, 0.001);
+    glColor3f(0.5, 0.5, 0.5);
+    glBegin(GL_QUADS);
+    glVertex2f(0, 0.05);
+    glVertex2f(0.5, 0.05);
+    glVertex2f(0.5, -0.05);
+    glVertex2f(0, -0.05);
+    glEnd();
+    glTranslatef(0, 0, -0.001);
     EGColor col = city.color.color;
     glColor3f(col.r, col.g, col.b);
-    glutSolidCube(1.0);
+    glTranslatef(0.3, -0.3, 0);
+    glutSolidCube(0.15);
+    glTranslatef(-0.3, 0, 0);
+    glutSolidCube(0.15);
+    glTranslatef(-0.3, 0, 0);
+    glutSolidCube(0.15);
+    glTranslatef(0, 0.6, 0);
+    glutSolidCube(0.15);
+    glTranslatef(0.3, 0, 0);
+    glutSolidCube(0.15);
+    glTranslatef(0.3, 0, 0);
+    glutSolidCube(0.15);
     glPopMatrix();
 }
 
