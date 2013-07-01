16439198c638d0ade2b159dee3d51f6c0c93d995
antonzherdev <anton.zherdev@gmail.com>
2013-07-01 15:44:51 +0400

    run test many times

--- Trains3D/Trains3D/Test/TRLevelSpec.m
+++ Trains3D/Trains3D/Test/TRLevelSpec.m
@@ -4,22 +4,29 @@
 SPEC_BEGIN(TRLevelSpec)
     describe(@"TRLevel", ^{
         EGMapSize mapSize = EGMapSizeMake(10, 7);
-        describe(@"should generare two cities in a time of starting", ^{
-            TRLevel* level = [TRLevel levelWithMapSize:mapSize];
-            [[[level cities] should] haveCountOf:2];
-            it(@"This cities should be generated on an edge of the map", ^{
-                [[level cities] forEach:^(TRCity* x) {
-                    EGMapPoint tile = x.tile;
-                    BOOL isPartial = egMapSsoIsPartialTile(mapSize, tile.x, tile.y);
-                    [[theValue(isPartial) should] beTrue];
-                }];
-            });
-            it(@"This cities should be generated in different tiles", ^{
-                NSSet *tilesSet = [[[level cities] map:^id(TRCity *x) {
-                    return val(x.tile);
-                }] set];
-                [[tilesSet should] haveCountOf:2];
-            });
+        it(@"should generare two cities in a time of starting.\n"
+                "This cities should be generated on an edge of the map.\n"
+                "This cities should be generated in different tiles.", ^{
+
+            for(int i = 0; i < 500; i++) {
+                @autoreleasepool {
+                    TRLevel* level = [TRLevel levelWithMapSize:mapSize];
+                    [[[level cities] should] haveCountOf:2];
+
+                    //This cities should be generated on an edge of the map.
+                    [[level cities] forEach:^(TRCity* x) {
+                        EGMapPoint tile = x.tile;
+                        BOOL isPartial = egMapSsoIsPartialTile(mapSize, tile.x, tile.y);
+                        [[theValue(isPartial) should] beTrue];
+                    }];
+
+                    //This cities should be generated in different tiles.
+                    NSSet *tilesSet = [[[level cities] map:^id(TRCity *x) {
+                        return val(x.tile);
+                    }] set];
+                    [[tilesSet should] haveCountOf:2];
+                }
+            }
         });
 
         it(@".createNewCity should create third city on the first call", ^{
