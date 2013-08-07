cb7b1d2586be89ea575c9b2d8d83dd59b93ccf86
Anton Zherdev <anton.zherdev@gmail.com>
2013-08-07 11:57:12 +0400

    lights

--- Trains3D/Trains3D/Processor/TRSwitchProcessor.m
+++ Trains3D/Trains3D/Processor/TRSwitchProcessor.m
@@ -18,7 +18,7 @@
     self = [super init];
     if(self) {
         _level = level;
-        _index = [EGRectIndex rectIndexWithRects:(@[tuple(val(EGRectMake(-0.1, 0.1, 0.3, 0.5)), tuple([TRRailConnector top], @NO)), tuple(val(EGRectMake(-0.1, 0.1, -0.5, -0.3)), tuple([TRRailConnector bottom], @NO)), tuple(val(EGRectMake(-0.5, -0.3, -0.1, 0.1)), tuple([TRRailConnector left], @NO)), tuple(val(EGRectMake(0.3, 0.5, -0.1, 0.1)), tuple([TRRailConnector right], @NO))])];
+        _index = [EGRectIndex rectIndexWithRects:(@[tuple(val(EGRectMake(-0.1, 0.1, 0.3, 0.5)), tuple([TRRailConnector top], @NO)), tuple(val(EGRectMake(-0.1, 0.1, -0.5, -0.3)), tuple([TRRailConnector bottom], @NO)), tuple(val(EGRectMake(-0.5, -0.3, -0.1, 0.1)), tuple([TRRailConnector left], @NO)), tuple(val(EGRectMake(0.3, 0.5, -0.1, 0.1)), tuple([TRRailConnector right], @NO)), tuple(val(EGRectMake(0.15, 0.25, 0.4, 0.5)), tuple([TRRailConnector top], @YES)), tuple(val(EGRectMake(-0.25, -0.15, -0.5, -0.3)), tuple([TRRailConnector bottom], @YES)), tuple(val(EGRectMake(-0.5, -0.4, 0.15, 0.25)), tuple([TRRailConnector left], @YES)), tuple(val(EGRectMake(0.4, 0.5, -0.25, -0.15)), tuple([TRRailConnector right], @YES))])];
         _downed = [CNOption none];
     }
     
@@ -33,8 +33,10 @@
     EGPoint location = [event location];
     EGPointI tile = egPointIApply(location);
     EGPoint relPoint = egPointSub(location, egPointApply(tile));
-    _downed = [[_index objectForPoint:relPoint] map:^id(CNTuple* _) {
-        return [_level.railroad contentInTile:tile connector:((TRRailConnector*)_.a)];
+    _downed = [[_index objectForPoint:relPoint] flatMap:^id(CNTuple* v) {
+        id content = [_level.railroad contentInTile:tile connector:((TRRailConnector*)v.a)];
+        if(unumb(v.b)) return [content asKindOfClass:[TRLight class]];
+        else return [content asKindOfClass:[TRSwitch class]];
     }];
     return [_downed isDefined];
 }
@@ -47,6 +49,9 @@
     if([_downed isDefined]) {
         [[((TRRailroadConnectorContent*)[_downed get]) asKindOfClass:[TRSwitch class]] forEach:^void(TRSwitch* _) {
             [_level tryTurnTheSwitch:_];
+        }];
+        [[((TRRailroadConnectorContent*)[_downed get]) asKindOfClass:[TRLight class]] forEach:^void(TRLight* _) {
+            [_ turn];
         }];
         return YES;
     } else {
