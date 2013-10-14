4ec0442551bd56b0e0b76f110124501ad5fb442f
Anton Zherdev <anton.zherdev@gmail.com>
2013-10-14 21:32:19 +0400

    16

--- Trains3D/Engine/Platform/iOS/GLPlatform.m
+++ Trains3D/Engine/Platform/iOS/GLPlatform.m
@@ -5,7 +5,7 @@ void egSaveTextureToFile(GLuint source, NSString* file) {
 }
 
 void egInitShadowTexture(GEVec2i size) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, size.x, size.y, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, size.x, size.y, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
