ce74f336e6390e8f1a513d39a7e698e8bb138994
Anton Zherdev <anton.zherdev@gmail.com>
2013-10-25 15:27:36 +0400

    #36

--- Trains3D/Engine/View/EGShadow.m
+++ Trains3D/Engine/View/EGShadow.m
@@ -760,7 +760,8 @@ static ODClassType* _EGShadowDrawShaderKey_type;
 
 - (NSString*)lightsCalculateVaryings {
     return [[[uintRange(_directLightCount) chain] map:^NSString*(id i) {
-        return [NSString stringWithFormat:@"dirLightShadowCoord%@ = (dirLightDepthMwcp%@ * vec4(position, 1)).xyz;", i, i];
+        return [NSString stringWithFormat:@"dirLightShadowCoord%@ = (dirLightDepthMwcp%@ * vec4(position, 1)).xyz;\n"
+            "dirLightShadowCoord%@.z -= 0.005;", i, i, i];
     }] toStringWithDelimiter:@"\n"];
 }
 
