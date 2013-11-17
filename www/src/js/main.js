22d2cec97e94f93f06848dc99aa1f5e33ccd2a94
Anton Zherdev <anton.zherdev@gmail.com>
2013-11-17 19:59:51 +0400

    optimize www

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -10,9 +10,9 @@
             arrows : 0,
             animation : 2,
             items: [
-                '<div class="txt">Build your railway between cities</div><img src="/img/scr0.jpg"/>',
-                '<div class="txt">Send trains to corresponding cities using lightnings and switches</div><img src="/img/scr1.jpg"/>',
-                '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg"/>'
+                '<div class="txt">Build your railway between cities</div><img src="/img/scr0.jpg" alt="Build your railway between cities"/>',
+                '<div class="txt">Send trains to corresponding cities using lightnings and switches</div><img src="/img/scr1.jpg" alt="Send trains to corresponding cities using lightnings and switches"/>',
+                '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg" alt="Prevent train crashes and fix railway damages"/>'
             ]
         });
     });
