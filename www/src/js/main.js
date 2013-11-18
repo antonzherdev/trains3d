f0f73fd9112ea8a80c1934248059dc574e373956
Anton Zherdev <anton.zherdev@gmail.com>
2013-11-18 19:02:35 +0400

    express

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -13,7 +13,8 @@
                 '<div class="txt">Build your railway between cities</div><img src="/img/scr0.jpg" alt="Build your railway between cities"/>',
                 '<div class="txt">Send trains to corresponding cities using lightnings and switches</div><img src="/img/scr1.jpg?1" alt="Send trains to corresponding cities using lightnings and switches"/>',
                 '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg" alt="Prevent train crashes and fix railway damages"/>',
-                '<div class="txt">Different locations and weather</div><img src="/img/scr3.jpg" alt="Different locations and weather"/>'
+                '<div class="txt">Different locations and weather</div><img src="/img/scr3.jpg" alt="Different locations and weather"/>',
+                '<div class="txt">Express trains are extremely fast</div><img src="/img/scr4.jpg" alt="Express trains are extremely fast"/>'
             ]
         });
     });
