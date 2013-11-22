e049a3d3965921d458735e707a2a6e21f1a084d3
Anton Zherdev <anton.zherdev@gmail.com>
2013-11-22 19:43:05 +0400

    www

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -15,10 +15,10 @@
                 '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg" alt="Prevent train crashes and fix railway damages"/>',
                 '<div class="txt">Different locations and weather</div><img src="/img/scr3.jpg" alt="Different locations and weather"/>',
                 '<div class="txt">Express trains are extremely fast</div><img src="/img/scr4.jpg" alt="Express trains are extremely fast"/>',
-                '<div class="txt">Use special interface for limited iPhone screen</div>' +
+                '<div class="txt">Use the special interface for the limited iPhone screen</div>' +
                     '<div class="iptxt" id="ip0">Zoom using pinch gesture</div>' +
                     '<img class="ipimg" id="ipi0" src="/img/iPhone0.jpg" alt="Zoom using pinch gesture"/>' +
-                    '<div class="iptxt" id="ip1">Build railway using one finger</div>' +
+                    '<div class="iptxt" id="ip1">Build rails using one finger</div>' +
                     '<img class="ipimg" id="ipi1" src="/img/iPhone1.jpg" alt="Build railway using one finger"/>' +
                     '<div class="iptxt" id="ip3">Scroll using two fingers</div>' +
                     '<img class="ipimg" id="ipi2" src="/img/iPhone2.jpg" alt="Scroll using two fingers"/>' +
