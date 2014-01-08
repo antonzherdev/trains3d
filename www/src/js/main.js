2fc7320823acf1c1cb95501dde40026480734ac0
Anton Zherdev <anton.zherdev@gmail.com>
2014-01-08 20:22:01 +0400

    www

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -10,12 +10,12 @@
             arrows : 0,
             animation : 2,
             items: [
-                '<div class="txt">Build your railway between cities</div><img src="/img/scr0.jpg?1" alt="Build your railway between cities"/>',
-                '<div class="txt">Send trains to corresponding cities using lightnings and switches</div><img src="/img/scr1.jpg?1" alt="Send trains to corresponding cities using lightnings and switches"/>',
-                '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg" alt="Prevent train crashes and fix railway damages"/>',
-                '<div class="txt">Beware of very fast express trains</div><img src="/img/scr4.jpg" alt="Express trains are extremely fast"/>',
-                '<div class="txt">Different locations</div><img src="/img/scr5.jpg" alt="Different locations"/>',
-                '<div class="txt">Different weather</div><img src="/img/scr3.jpg" alt="Different weather"/>'
+                '<div class="txt">Build your railway between cities</div><img src="/img/scr0.jpg?2" alt="Build your railway between cities"/>',
+                '<div class="txt">Send trains to corresponding cities using lightnings and switches</div><img src="/img/scr1.jpg?2" alt="Send trains to corresponding cities using lightnings and switches"/>',
+                '<div class="txt">Prevent train crashes and fix railway damages</div><img src="/img/scr2.jpg?2" alt="Prevent train crashes and fix railway damages"/>',
+                '<div class="txt">Beware of very fast express trains</div><img src="/img/scr4.jpg?2" alt="Express trains are extremely fast"/>',
+                '<div class="txt">Different locations</div><img src="/img/scr5.jpg?2" alt="Different locations"/>',
+                '<div class="txt">Different weather</div><img src="/img/scr3.jpg?2" alt="Different weather"/>'
             ]
         });
     });
